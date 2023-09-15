81c11100ce5150980c07be2b7f67fb9dbf8d2f37
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2023-09-15 17:34:27 -0300

    feat: User and Role entities created

---
+++ Src/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class DataContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+}
