e2749743d74dacadb3e04bd351a0e86430c07671
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2023-09-15 12:30:44 -0300

    first commit

---
+++ Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+public class BaseApiController : ControllerBase
+{
+    
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+}
